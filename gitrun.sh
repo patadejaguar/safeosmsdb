7875cbaf00a606dde2c2eedd7b45670c37923011
patadejaguar <[patadejaguar@gmail.com]>
2018-04-02 09:28:21 -0500

    add portuguese trad

--- gitrun.sh
+++ gitrun.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+REPO="pt"
+
 git init
 git add *
 git commit -m "$1"
 git remote add origin https://github.com/patadejaguar/safeosmsdb.git
-git fetch -u origin master
-git push -u origin master
+
+#If exists the repo
+git fetch -u origin $REPO
+
+git push -u origin $REPO
 
 exit 0;
